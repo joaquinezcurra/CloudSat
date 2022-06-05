a39b2b45e7bc6c477e19cb5f3be774ff10a218fe
Joaquin Ezcurra <bongotai@users.noreply.github.com>
2022-06-05 13:39:53 -0300

    Initial Upload. Python File w/ CCPLOT commands

---
+++ ccplot-test.py
@@ -0,0 +1,158 @@
+ccplot -d 150 -x 0..4 -y 0..1 -c cloudsat-reflectivity.cmap -o cloudsat-reflec.pdf cloudsat-reflec
+
+ccplot -d 150 -c cloudsat-reflectivity.cmap -o cloudsat-reflec.pdf cloudsat-reflec 
+
+ccplot -o cloudsat2.pdf -x 0..1000 -c /Users/joaquinx/ccplot-1.5.6/cmap/cloudsat-reflectivity.cmap cloudsat-reflec /Users/joaquinx/Documents/ART1/Produktion/Cloudsat/2006224184641_01550_CS_2B-GEOPROF_GRANULE_P_R04_E01.hdf
+
+ccplot -o cloudsat3.pdf -x 1000..1500 -c /Users/joaquinx/ccplot-1.5.6/cmap/cloudsat-reflectivity.cmap cloudsat-reflec /Users/joaquinx/Documents/ART1/Produktion/Cloudsat/2006224184641_01550_CS_2B-GEOPROF_GRANULE_P_R04_E01.hdf
+
+ccplot -o cloudsat4.pdf -x 3000..3700 -c /Users/joaquinx/ccplot-1.5.6/cmap/cloudsat-reflectivity.cmap cloudsat-reflec /Users/joaquinx/Documents/ART1/Produktion/Cloudsat/2006224184641_01550_CS_2B-GEOPROF_GRANULE_P_R04_E01.hdf
+
+ccplot -d 1200 -o cloudsat5.pdf -x 3700..4700 -c /Users/joaquinx/ccplot-1.5.6/cmap/cloudsat-reflectivity.cmap cloudsat-reflec /Users/joaquinx/Documents/ART1/Produktion/Cloudsat/2006224184641_01550_CS_2B-GEOPROF_GRANULE_P_R04_E01.hdf
+
+ccplot -r 240 -d 600 -o cloudsat6.pdf -x 3700..4700 -c /Users/joaquinx/ccplot-1.5.6/cmap/cloudsat-reflectivity.cmap cloudsat-reflec /Users/joaquinx/Documents/ART1/Produktion/Cloudsat/2006224184641_01550_CS_2B-GEOPROF_GRANULE_P_R04_E01.hdf
+
+ccplot -r 1 -d 600 -o cloudsat7.pdf -x 3700..4700 -c /Users/joaquinx/ccplot-1.5.6/cmap/cloudsat-reflectivity.cmap cloudsat-reflec /Users/joaquinx/Documents/ART1/Produktion/Cloudsat/2006224184641_01550_CS_2B-GEOPROF_GRANULE_P_R04_E01.hdf
+
+ccplot -d 600 -o cloudsat8.eps -x 3700..4700 -c /Users/joaquinx/ccplot-1.5.6/cmap/cloudsat-reflectivity.cmap cloudsat-reflec /Users/joaquinx/Documents/ART1/Produktion/Cloudsat/2006224184641_01550_CS_2B-GEOPROF_GRANULE_P_R04_E01.hdf
+
+ccplot -d 600 -o cloudsat8.png -x 3700..4700 -c /Users/joaquinx/ccplot-1.5.6/cmap/cloudsat-reflectivity.cmap cloudsat-reflec /Users/joaquinx/Documents/ART1/Produktion/Cloudsat/2006224184641_01550_CS_2B-GEOPROF_GRANULE_P_R04_E01.hdf
+
+ccplot -d 600 -o cloudsat8.png -x 3700..4700 -c /Users/joaquinx/ccplot-1.5.6/cmap/cloudsat-reflectivity.cmap cloudsat-reflec /Users/joaquinx/Documents/ART1/Produktion/Cloudsat/2006153201756_00517_CS_1B-CPR_GRANULE_P_R04_E00.hdf
+__________________________________________________________
+
+ccplot -c cloudsat-reflectivity.cmap -a 15 -x 24.60S..31S,50W..60W -y -1000..18000 cloudsat-reflec
+
+ccplot-1.5.6 % ccplot -i /Users/joaquinx/Documents/ART1/Produktion/Cloudsat/Processing\ with\ ccplot/2009037050924_14779_CS_2B-GEOPROF_GRANULE_P_R04_E02.hdf
+# Type: CloudSat
+# Subtype: 2B-GEOPROF
+# Time: 2009-02-06 05:09:24, 2009-02-06 06:48:16
+# Height: -4819m, 24920m
+# nray: 37082
+# nbin: 125
+# Longitude: 179.98W, 179.97E
+# Latitude: 81.79S, 81.79N
+
+ccplot -c cloudsat-reflectivity.cmap -r 3 -a 14 -x 24.60S..31S,50W..60W -y -1000..18000 cloudsat-reflec
+
+ccplot -o cloudsat1.pdf -x 0..1000 cloudsat-reflec
+
+ccplot -c cloudsat-reflectivity-III.cmap -r 3 -a 14 -x 0..3000 cloudsat-reflec
+
+ccplot -c cloudsat-reflectivity-III.cmap -r 3 -a 14 -x 0..4000 cloudsat-reflec
+
+# para 2006335162451_03165_CS_2B-GEOPROF_GRANULE_P_R04_E02
+ccplot -c cloudsat-reflectivity-III.cmap -r 3 -a 7 -x 35300..36000 cloudsat-reflec
+ccplot -c cloudsat-reflectivity-III.cmap -r 3 -a 7 -x 36500..37080 cloudsat-reflec
+
+
+ccplot -c cloudsat-reflectivity-III.cmap -r 3 -a 14 -x 300..1000 cloudsat-reflec
+
+ccplot -c cloudsat-reflectivity-III.cmap -r 3 -a 14 -x 0..1000 cloudsat-reflec 2006153134223_00513_CS_2B-GEOPROF_GRANULE_P1_R05_E00_F00.hdf
+# Invalid vertical extent
+
+ccplot -c cloudsat-reflectivity-III.cmap -r 3 -a 15 -x 81.79S..81.82N,180.00W..170W -y -1000..18000 cloudsat-reflec /Users/joaquinx/Documents/ART1/Produktion/Cloudsat/ccplot\ processing/HDF\ files/153/2006153134223_00513_CS_2B-GEOPROF_GRANULE_P1_R05_E00_F00.hdf cloudsat-reflec 2006153134223_00513_CS_2B-GEOPROF_GRANULE_P1_R05_E00_F00.hdf
+# result ok
+
+# Lorenzo
+ccplot -c cloudsat-reflectivity-III.cmap -r 3 -a 14 -x 0..1000 cloudsat-reflec /Users/joaquinx/Documents/ART1/Produktion/Cloudsat/ccplot\ processing/HDF\ files/CS_TC-ATL-13L-LORENZO_GRANULE.tar/2019266142131_71407_CS_TC-ATL-13L-LORENZO_GRANULE/2019266142131_71407_CS_2D_TC-ATL-13L\:LORENZO_GRANULE.hdf
+# ccplot: Unknown product type
+
+# Find out about direct file DL
+wget -nd -N -r --user=<your username> --ask-password
+wget -nd -N -r --user=bongotai --ask-password ftp://ftp.cloudsat.cira.colostate.edu/2B-GEOPROF.P1_R05/2015/001
+
+# 2022 example from ccplot help file
+ccplot -c calipso.cmap -x 11000..13000 -y 0..25000 -o out.png calipso532
+
+__________________________________________________________
+
+# ccplot help file
+Plot data from CloudSat, CALIPSO and Aqua MODIS product files.
+Example: ccplot -c calipso.cmap -x 11000..13000 -y 0..25000 -o out.png calipso532 CAL_LID_L1-Prov-V2-01.2006-08-12T19-15-34ZD.hdf
+
+Where OPTION is one of:
+  -a RATIO                      aspect ratio of profile and layer plots in
+                                km horizontal per km vertical
+  -c FILE                       colormap file
+  -d DPI                        DPI of the output file
+  -h                            print help information and exit
+  -i FILE                       print information about FILE
+  -m BAND                       MODIS band (e.g. r1 for reflectance band 1,
+                                x31 for radiance band 31)
+  -o OUTFILE                    output file, type is determined by extension
+  -p PROJECTION[:PROJOPTS]      projection type and options
+  -r RADIUS                     interpolation radius
+  -v                            verbose mode
+  -V                            print version information and exit
+  -x FROM..TO[,FROM..TO]        horizontal extent
+  -y FROM..TO                   vertical extent in meters
+  -z OPTION=VAL[,OPTION=VAL]    list of option-value pairs
+
+OPTION is one of:
+  Use -z help for a list of available options. 
+
+PROJECTION is one of:
+  Use -p help for a list of available projections.
+
+PROJOPTS is a list of option value pairs, where option is one of:
+  boudinglat
+  lat_0
+  lat_1
+  lat_2
+  lat_ts
+  lon_0
+
+TYPE is one of:
+  cloudsat-reflec
+  calipso532
+  calipso532p
+  calipso1064
+  calipso-cratio
+  calipso-dratio
+  calipso532-layer
+  calipso1064-layer
+  calipso-cratio-layer
+  calipso-dratio-layer
+  calipso-temperature-layer
+  orbit
+  orbit-clipped
+
+FILE is a CloudSat or Aqua MODIS HDF-EOS2 file, or a CALIPSO HDF4 file.
+
+__________________________________________________________
+
+ccplot -c calipso.cmap -x 11000..13000 -y 0..25000 -o out.png calipso532
+
+ccplot -c cloudsat-reflectivity-III.cmap -x 11000..13000 -y 0..25000 -o OUT1.png cloudsat-reflec
+
+ccplot -c cloudsat-reflectivity-III.cmap -r 1 -o OUT1.png cloudsat-reflec /Users/joaquinx/Documents/ART1/Produktion/Cloudsat/ccplot\ processing/2022/2006255045242_01993_CS_TC-ATL-06L-FLORENCE_GRANULE/2006255045242_01993_CS_2B-GEOPROF_GRANULE_P1_R05_E02_F00_ATL-06L\:FLORENCE.hdf
+
+ccplot -c cloudsat-reflectivity-III.cmap -r 20 -o OUT1.png cloudsat-reflec /Users/joaquinx/Documents/ART1/Produktion/Cloudsat/ccplot\ processing/2022/2006255045242_01993_CS_TC-ATL-06L-FLORENCE_GRANULE/2006255045242_01993_CS_2B-GEOPROF_GRANULE_P1_R05_E02_F00_ATL-06L\:FLORENCE.hdf
+# cambiar el valor de R no modifica el resultado visualmente
+
+__________________________________________________________
+
+# Color Maps
+
+ccplot -c cloudsat-reflectivity.cmap -o OUT1.png cloudsat-reflec /Users/joaquinx/Documents/ART1/Produktion/Cloudsat/ccplot\ processing/2022/2006255045242_01993_CS_TC-ATL-06L-FLORENCE_GRANULE/2006255045242_01993_CS_2B-GEOPROF_GRANULE_P1_R05_E02_F00_ATL-06L\:FLORENCE.hdf
+
+ccplot -c cloudsat-reflectivity-I.cmap -o OUT1.png cloudsat-reflec /Users/joaquinx/Documents/ART1/Produktion/Cloudsat/ccplot\ processing/2022/2006255045242_01993_CS_TC-ATL-06L-FLORENCE_GRANULE/2006255045242_01993_CS_2B-GEOPROF_GRANULE_P1_R05_E02_F00_ATL-06L\:FLORENCE.hdf
+
+ccplot -c cloudsat-reflectivity-II.cmap -o OUT1.png cloudsat-reflec /Users/joaquinx/Documents/ART1/Produktion/Cloudsat/ccplot\ processing/2022/2006255045242_01993_CS_TC-ATL-06L-FLORENCE_GRANULE/2006255045242_01993_CS_2B-GEOPROF_GRANULE_P1_R05_E02_F00_ATL-06L\:FLORENCE.hdf
+
+ccplot -c cloudsat-reflectivity-III.cmap -o OUT1.png cloudsat-reflec /Users/joaquinx/Documents/ART1/Produktion/Cloudsat/ccplot\ processing/2022/2006255045242_01993_CS_TC-ATL-06L-FLORENCE_GRANULE/2006255045242_01993_CS_2B-GEOPROF_GRANULE_P1_R05_E02_F00_ATL-06L\:FLORENCE.hdf
+
+ccplot -c cloudsat-reflectivity-IV.cmap -o OUT1.png cloudsat-reflec /Users/joaquinx/Documents/ART1/Produktion/Cloudsat/ccplot\ processing/2022/2006255045242_01993_CS_TC-ATL-06L-FLORENCE_GRANULE/2006255045242_01993_CS_2B-GEOPROF_GRANULE_P1_R05_E02_F00_ATL-06L\:FLORENCE.hdf
+
+ccplot -c cloudsat-reflectivity-V.cmap -o OUT1.png cloudsat-reflec /Users/joaquinx/Documents/ART1/Produktion/Cloudsat/ccplot\ processing/2022/2006255045242_01993_CS_TC-ATL-06L-FLORENCE_GRANULE/2006255045242_01993_CS_2B-GEOPROF_GRANULE_P1_R05_E02_F00_ATL-06L\:FLORENCE.hdf
+
+
+__________________________________________________________
+
+ccplot -c cloudsat-reflectivity-III.cmap -o OUT1.png -a 28.0 cloudsat-reflec /Users/joaquinx/Documents/ART1/Produktion/Cloudsat/ccplot\ processing/2022/2006255045242_01993_CS_TC-ATL-06L-FLORENCE_GRANULE/2006255045242_01993_CS_2B-GEOPROF_GRANULE_P1_R05_E02_F00_ATL-06L\:FLORENCE.hdf
+# -a es el ratio de altura anchura. 14 es 1:1. 7 es el doble de largo. 28 es el doble de alto
+
+ccplot -c cloudsat-reflectivity-III.cmap -o OUT1.png -d 900 cloudsat-reflec /Users/joaquinx/Documents/ART1/Produktion/Cloudsat/ccplot\ processing/2022/2006255045242_01993_CS_TC-ATL-06L-FLORENCE_GRANULE/2006255045242_01993_CS_2B-GEOPROF_GRANULE_P1_R05_E02_F00_ATL-06L\:FLORENCE.hdf
+ccplot -c cloudsat-reflectivity-III.cmap -o OUT1.png -d 600 cloudsat-reflec /Users/joaquinx/Documents/ART1/Produktion/Cloudsat/ccplot\ processing/2022/2006255045242_01993_CS_TC-ATL-06L-FLORENCE_GRANULE/2006255045242_01993_CS_2B-GEOPROF_GRANULE_P1_R05_E02_F00_ATL-06L\:FLORENCE.hdf
+ccplot -c cloudsat-reflectivity-III.cmap -o OUT1.png -d 100 cloudsat-reflec /Users/joaquinx/Documents/ART1/Produktion/Cloudsat/ccplot\ processing/2022/2006255045242_01993_CS_TC-ATL-06L-FLORENCE_GRANULE/2006255045242_01993_CS_2B-GEOPROF_GRANULE_P1_R05_E02_F00_ATL-06L\:FLORENCE.hdf
